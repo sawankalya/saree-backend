178644816e2c7a3a5a38b06e43d0222ad1b8482b
sawankalya <121507065+sawankalya@users.noreply.github.com>
2026-02-02 00:55:21 +0530

    Increase body size limit for image upload

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+
+app.use(express.json({ limit: "15mb" }));
+app.use(express.urlencoded({ limit: "15mb", extended: true }));
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"));
